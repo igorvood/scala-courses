328c50fa24a1c8e343bb8fa92538784ed03559ea
vood <balitsky_igor@list.ru>
2021-07-04 14:43:20 +0300

    q

--- Stepik/SeaBattle/src/main/scala/data/Naval.scala
+++ Stepik/SeaBattle/src/main/scala/data/Naval.scala
@@ -8,4 +8,32 @@ object Naval {
   type Ship = List[Point] // Корабль как список точек
   type Fleet = Map[String, Ship] // Множество всех кораблей на поле как ассоциативный массив. По строковому ключу (имени корабля) находится список точек корабля
   type Game = (Field, Fleet) // Игровое поле и список кораблей
+
+
+  var input = List(
+    "3",
+    "BlackPearl 3",
+    "1 6",
+    "1 7",
+    "1 8",
+    "MillenniumFalcon 4",
+    "2 5",
+    "3 5",
+    "4 5",
+    "5 5",
+    "Varyag 1",
+    "9 9")
+
+
+  def readNext: String = {
+    input match {
+      case x :: xs => {
+        input = xs
+        x
+      }
+      case Nil => "Empty"
+    }
+
+  }
+
 }
