e2eb29776d57f1b79eaf5992c061c0f27d61857e
vood <balitsky_igor@list.ru>
2021-07-04 14:41:05 +0300

    q

---
+++ Stepik/SeaBattle/src/main/scala/Main.scala
@@ -0,0 +1,7 @@
+package ru.vood.scala.sea.battle
+
+object Main extends App {
+
+//  val zeroGame: Game = (field, Map())
+  // здесь Ваш код
+}
---
+++ Stepik/SeaBattle/src/main/scala/data/Lesson.scala
@@ -0,0 +1,17 @@
+package ru.vood.scala.sea.battle
+package data
+
+object Lesson {
+  val field = Vector(
+    Vector(false, false, false, false, false, false, false, false, false, false),
+    Vector(false, false, false, false, false, false, false, false, false, false),
+    Vector(false, false, false, false, false, false, false, false, false, false),
+    Vector(false, false, false, false, false, false, false, false, false, false),
+    Vector(false, false, false, false, false, false, false, false, false, false),
+    Vector(false, false, false, false, false, false, false, false, false, false),
+    Vector(false, false, false, false, false, false, false, false, false, false),
+    Vector(false, false, false, false, false, false, false, false, false, false),
+    Vector(false, false, false, false, false, false, false, false, false, false),
+    Vector(false, false, false, false, false, false, false, false, false, false)
+  )
+}
