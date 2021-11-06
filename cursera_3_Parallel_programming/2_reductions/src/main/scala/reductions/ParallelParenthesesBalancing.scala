b181a28b92b6d2bd46bc8ae612c6e17dc894fb63
vood <balitsky_igor@list.ru>
2021-11-06 23:03:43 +0300

    d

--- cursera_3_Parallel_programming/2_reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
+++ cursera_3_Parallel_programming/2_reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
@@ -1,7 +1,8 @@
 package reductions
 
-import scala.annotation.*
 import org.scalameter.*
+
+import scala.annotation.*
 
 object ParallelParenthesesBalancingRunner:
 
@@ -14,7 +15,7 @@ object ParallelParenthesesBalancingRunner:
     Key.exec.maxWarmupRuns := 80,
     Key.exec.benchRuns := 120,
     Key.verbose := false
-  ) withWarmer(Warmer.Default())
+  ) withWarmer (Warmer.Default())
 
   def main(args: Array[String]): Unit =
     val length = 100000000
@@ -33,35 +34,64 @@ object ParallelParenthesesBalancingRunner:
     println(s"parallel balancing time: $fjtime")
     println(s"speedup: ${seqtime.value / fjtime.value}")
 
-object ParallelParenthesesBalancing extends ParallelParenthesesBalancingInterface:
+object ParallelParenthesesBalancing extends ParallelParenthesesBalancingInterface :
 
-  /** Returns `true` iff the parentheses in the input `chars` are balanced.
+  def code(ch: Char): Int = ch match {
+    case '(' => 1
+    case ')' => -1
+    case _ => 0
+  }
+
+/** Returns `true` iff the parentheses in the input `chars` are balanced.
    */
   def balance(chars: Array[Char]): Boolean = {
-    def loop(level: Int, chars: Array[Char]): Int = {
-      if (chars.isEmpty) level
-      else if (level < 0) level
-      else if (chars.head == '(') loop(level + 1, chars.tail)
-      else if (chars.head == ')') loop(level - 1, chars.tail)
-      else loop(level, chars.tail)
+    @tailrec
+    def loop(chLs: List[Char], acc: Int = 0): Int = chLs match {
+      case head :: tail if acc >= 0 => loop(tail, acc + code(head))
+      case _ => acc
     }
-    loop(0, chars) == 0
+
+    loop(chars.toList) == 0
+
   }
 
   /** Returns `true` iff the parentheses in the input `chars` are balanced.
    */
-  def parBalance(chars: Array[Char], threshold: Int): Boolean =
+  def parBalance(chars: Array[Char], threshold: Int): Boolean = {
 
-    def traverse(idx: Int, until: Int, arg1: Int, arg2: Int) /*: ???*/ = {
-      ???
+    @tailrec
+    def traverse(idx: Int, until: Int, arg1: Int, arg2: Int): (Int, Int) = {
+      if (idx < until) {
+        chars(idx) match {
+          case '(' => traverse(idx + 1, until, arg1 + 1, arg2)
+          case ')' =>
+            if (arg1 > 0) traverse(idx + 1, until, arg1 - 1, arg2)
+            else traverse(idx + 1, until, arg1, arg2 + 1)
+          case _ => traverse(idx + 1, until, arg1, arg2)
+        }
+      } else (arg1, arg2)
     }
 
-    def reduce(from: Int, until: Int) /*: ???*/ = {
-      ???
+    def reduce(from: Int, until: Int): (Int, Int) = {
+      val size = until - from
+      if (size > threshold) {
+        val halfSize = size / 2
+        val ((a1, a2), (b1, b2)) = parallel(reduce(from, from + halfSize), reduce(from + halfSize, until))
+        val res: (Int, Int) = if (a1 > b2) {
+          ((a1 - b2 + b1) , a2)
+        } else {
+          b1 -> (b2 - a1 + a2)
+        }
+        res
+      }
+      else {
+        traverse(from, until, 0, 0)
+      }
     }
 
-    reduce(0, chars.length) == ???
+    reduce(0, chars.length) == (0, 0)
+  }
 
-  // For those who want more:
-  // Prove that your reduction operator is associative!
+// For those who want more:
+// Prove that your reduction operator is associative!
 
